15b8e009b581c45e4d79c8276c5d0f391fb01ed1
Roman Drahunov <104847647+RomanDrahunovScalr@users.noreply.github.com>
2025-05-09 15:02:08 +0300

    Update main.tf

--- terraform-providers/main.tf
+++ terraform-providers/main.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     null = {
       source = "hashicorp/null"
-      version = "3.2.0"
+      version = "3.2.1"
     }
   }
 }
 
-resource "null_resource" "duplicate" {
+resource "null_resource" "single" {
   provisioner "local-exec" {
     command = "echo $ENV"
     environment = {
